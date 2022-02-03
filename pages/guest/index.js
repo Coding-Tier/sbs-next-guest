dcd366084719b447a1dc59ee52581b4df976d60d
Ammar Lakhani <ammarnaeem77@gmail.com>
2022-02-03 21:18:58 +0500

    added react-loadingg dep

--- pages/guest/index.js
+++ pages/guest/index.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import { SemipolarLoading } from "react-loadingg";
-import { Grid, Button } from "@material-ui/core";
-import Bone2Image from "../assets/bone1_Bitmap.png";
-import MyContext from '../helper/themeContext';
+// import { SemipolarLoading } from "react-loadingg";
+// import { Grid, Button } from "@material-ui/core";
+// import Bone2Image from "../assets/bone1_Bitmap.png";
+// import MyContext from '../helper/themeContext';
 
 class Guest extends Component {
     constructor(props) {
