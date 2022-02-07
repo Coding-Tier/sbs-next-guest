d4ecb080957e832e0578317c8d354bc6bf1f59bf
Ammar Lakhani <ammarnaeem77@gmail.com>
2022-02-08 00:41:24 +0500

    removed-security-header

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 const withImages = require('next-images')
 
 module.exports = withImages({
+    async headers() {
+      return [
+        {
+          source: '/',
+          headers: [
+            {
+              key: 'Content-Security-Policy',
+              value: "frame-ancestors https://www.hipandkneestepbystep.com/sbs-education",
+            },
+          ],
+        }
+      ]
+    },
     images: {
       disableStaticImages: true
     }
