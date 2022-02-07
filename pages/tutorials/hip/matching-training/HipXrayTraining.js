b9a0cb8f4b9d698de91ab72e9a3d01d85b096390
Ammar Lakhani <ammarnaeem77@gmail.com>
2022-02-07 20:27:38 +0500

    a

--- pages/tutorials/hip/matching-training/HipXrayTraining.js
+++ pages/tutorials/hip/matching-training/HipXrayTraining.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
 import Welcome from "./welcome";
 import { Route, Router, Redirect } from "react-router-dom";
-import MyContext from "../../helper/themeContext";
+import MyContext from "../../../helper/themeContext";
 // import MatchingVideo from './matchingVideo'
 // import MatchingVideo2 from './matchingVideo2'
 
 // import LGS from './LGS';
-import MatchingTutorial from "./matchingTutorial/matchingTutorial";
+import MatchingTutorial from "./matching-tutorial";
 import Congratulations from "./congratulations";
 
 class HipXrayTraining extends Component {
