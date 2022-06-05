7789eae454e91b0441a0d4dd6050e1cca38d2772
Ammar Lakhani <ammarnaeem77@gmail.com>
2022-06-05 05:24:10 +0500

    fix

--- pages/tutorials/knee/short-knee/indexOld.js
+++ pages/tutorials/knee/short-knee/indexOld.js
@@ -1,41 +1,41 @@
-import React, { Component } from 'react';
-import { Route, Router } from "react-router-dom";
-import MyContext from '../../../helper/themeContext';
-import Congrats from './Congrats';
-import COTK2 from './COTK2';
-import EVALKV from './EVALKV';
-import JSNKC from './JSNKC';
-import JSNLC from './JSNLC';
-import JSNMC from './JSNMC';
+// import React, { Component } from 'react';
+// import { Route, Router } from "react-router-dom";
+// import MyContext from '../../../helper/themeContext';
+// import Congrats from './Congrats';
+// import COTK2 from './COTK2';
+// import EVALKV from './EVALKV';
+// import JSNKC from './JSNKC';
+// import JSNLC from './JSNLC';
+// import JSNMC from './JSNMC';
 
-import LATCOTK from './LATCOTK';
-import LATMALCOTKITX from './LATMALCOTKITX';
-import SampleKneeEval from './Sample-knee-eval';
-import Welcome from './Welcome';
+// import LATCOTK from './LATCOTK';
+// import LATMALCOTKITX from './LATMALCOTKITX';
+// import SampleKneeEval from './Sample-knee-eval';
+// import Welcome from './Welcome';
 
-class ShortKnee extends Component {
-    constructor(props) {
-        super(props);
-    }
-    state = {  }
-    render() { 
-        return (
-        <Router history={this.context.history}>
+// class ShortKnee extends Component {
+//     constructor(props) {
+//         super(props);
+//     }
+//     state = {  }
+//     render() { 
+//         return (
+//         <Router history={this.context.history}>
 
-            <Route exact path="/tutorials/knee/short-knee/" component={Welcome} />
-            <Route exact path="/tutorials/knee/short-knee/welcome" component={Welcome} />
-            <Route exact path="/tutorials/knee/short-knee/LATCOTK" component={LATCOTK} />
-            <Route exact path="/tutorials/knee/short-knee/EVALKV" component={EVALKV} />
-            <Route exact path="/tutorials/knee/short-knee/LATMALCOTKITX" component={LATMALCOTKITX} />
-            <Route exact path="/tutorials/knee/short-knee/COTK2" component={COTK2} />
-            <Route exact path="/tutorials/knee/short-knee/JSNMC" component={JSNMC} />
-            <Route exact path="/tutorials/knee/short-knee/JSNLC" component={JSNLC} />
-            <Route exact path="/tutorials/knee/short-knee/JSNKC" component={JSNKC} />
-            <Route exact path="/tutorials/knee/short-knee/Sample-knee-eval" component={SampleKneeEval} />
-            <Route exact path="/tutorials/knee/short-knee/Congrats" component={Congrats} />
-        </Router> );
-    }
-}
+//             <Route exact path="/tutorials/knee/short-knee/" component={Welcome} />
+//             <Route exact path="/tutorials/knee/short-knee/welcome" component={Welcome} />
+//             <Route exact path="/tutorials/knee/short-knee/LATCOTK" component={LATCOTK} />
+//             <Route exact path="/tutorials/knee/short-knee/EVALKV" component={EVALKV} />
+//             <Route exact path="/tutorials/knee/short-knee/LATMALCOTKITX" component={LATMALCOTKITX} />
+//             <Route exact path="/tutorials/knee/short-knee/COTK2" component={COTK2} />
+//             <Route exact path="/tutorials/knee/short-knee/JSNMC" component={JSNMC} />
+//             <Route exact path="/tutorials/knee/short-knee/JSNLC" component={JSNLC} />
+//             <Route exact path="/tutorials/knee/short-knee/JSNKC" component={JSNKC} />
+//             <Route exact path="/tutorials/knee/short-knee/Sample-knee-eval" component={SampleKneeEval} />
+//             <Route exact path="/tutorials/knee/short-knee/Congrats" component={Congrats} />
+//         </Router> );
+//     }
+// }
  
-export default ShortKnee;
-ShortKnee.contextType = MyContext
+// export default ShortKnee;
+// ShortKnee.contextType = MyContext
